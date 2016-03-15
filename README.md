b36e94fe73d58b207710abb8af50258a0b98d222
cesardeazevedo <cesardeazevedo@outlook.com>
2016-03-15 17:17:12 -0300

    Updates

---
+++ README.md
@@ -0,0 +1,87 @@
+[blinktrade.com](http://blinktrade.com)
+=============
+
+Blinktrade Landing Page
+
+## How it works?
+
+We use [jekyll](http://jekyllrb.com/), a static generator in Ruby, to create this website.
+
+## First Steps
+
+1 - Install [Git](http://git-scm.com/downloads) and [Ruby](https://www.ruby-lang.org/pt/downloads/), in case you don't have them yet.
+
+2 - Once installed these dependecies, open up the terminal and install [Jekyll](http://jekyllrb.com) with the following commands.
+
+```sh
+$ gem install jekyll
+```
+
+3 - Now clone the project
+```sh
+$ git clone git@github.com:blinktrade/blinktrade.github.io.git
+```
+
+4 - Navigate to the project folder
+```sh
+$ cd blinktrade.github.io
+```
+
+5 - And finally run: 
+```sh
+$ jekyll server --watch
+```
+
+You'll have access to the website at `localhost:4000` :D
+
+## Browser Support
+
+![IE](https://raw.githubusercontent.com/alrra/browser-logos/master/internet-explorer/internet-explorer_64x64.png "Internet Explorer") | ![Chrome](https://raw.githubusercontent.com/alrra/browser-logos/master/chrome/chrome_64x64.png "Google Chrome") | ![Firefox](https://raw.githubusercontent.com/alrra/browser-logos/master/firefox/firefox_64x64.png "Firefox") | ![Opera](https://raw.githubusercontent.com/alrra/browser-logos/master/opera/opera_64x64.png "Opera") | ![Safari](https://raw.githubusercontent.com/alrra/browser-logos/master/safari/safari_64x64.png "Safari")
+|:---:|:---:|:---:|:---:|:---:|
+| IE 8+ ✔ | Latest ✔ | Latest ✔ | Latest ✔ | Latest ✔ |
+
+## File Structure
+
+The file structure for the project is organized in the following way:
+
+```
+.
+|-- _includes
+|-- _layouts
+|-- _posts
+|-- _config.yml
+`-- index.html
+```
+
+### [_includes](https://github.com/blinktrade/blinktrade.github.io/_includes)
+
+They're blocks of code used to generate the main page of the site (index.html).
+
+### [_layouts](https://github.com/blinktrade/blinktrade.github.io/_layouts)
+
+Here you'll find the default template of the application.
+
+### [_posts](https://github.com/blinktrade/blinktrade.github.io/_posts)
+
+Here you'll find a list of files for each post.
+
+### [_config.yml](https://github.com/blinktrade/blinktrade.github.io/_config.yml)
+
+It stores most of the settings of the application.
+
+### [index.html](https://github.com/blinktrade/blinktrade.github.io/index.html)
+
+It's the file responsible for all application sections.
+
+_More information about Jekyll's file structure [here](https://github.com/mojombo/jekyll/wiki/Usage)._
+
+## Contributing
+ 
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## License
+[GNU GENERAL PUBLIC LICENSE](https://github.com/randsonjs/blinktrade.github.io/blob/master/LICENSE) © Blinktrade, Inc.
---
+++ assets/js/main.js
@@ -0,0 +1,148 @@
+(function ($) {
+  "use strict";
+
+  function onScrollAnimations() {
+    $('.wp-1').waypoint(function() {
+
+      var delay = 0;
+      $.each($('.wp-sequence-1'), function(index, btn){
+        setTimeout(function(){
+          $(btn).addClass('animated fadeInUp');
+        }, delay);
+        delay += 750;
+      });
+
+    }, {
+      offset: '75%'
+    });
+    $('.wp-2').waypoint(function() {
+      var delay = 0;
+      $.each($('.wp-sequence-2'), function(index, btn){
+        setTimeout(function(){
+          $(btn).addClass('animated fadeInUp');
+        }, delay);
+        delay += 300;
+      });
+    }, {
+      offset: '75%'
+    });
+    $('.wp-3').waypoint(function() {
+      $('.wp-3').addClass('animated fadeInUp');
+    }, {
+      offset: '75%'
+    });
+    $('.wp-4').waypoint(function() {
+      $('.wp-4').addClass('animated fadeIn');
+    }, {
+      offset: '75%'
+    });
+    $('.wp-5').waypoint(function() {
+      $('.wp-5').addClass('animated fadeInRight');
+    }, {
+      offset: '50%'
+    });
+    $('.wp-6').waypoint(function() {
+      $('.wp-6').addClass('animated fadeInLeft');
+    }, {
+      offset: '100%'
+    });
+    $('.wp-7').waypoint(function() {
+      $('.wp-7').addClass('animated fadeInUp');
+    }, {
+      offset: '60%'
+    });
+    $('.wp-8').waypoint(function() {
+      $('.wp-8').addClass('animated fadeInUp');
+    }, {
+      offset: '60%'
+    });
+  }
+
+  function navMobileCollapse() {
+    // avoid having both mobile navs opened at the same time
+    $('#collapsingMobileUser').on('show.bs.collapse', function () {
+      $('#collapsingNavbar').removeClass('in');
+      $('[data-target="#collapsingNavbar"]').attr('aria-expanded', 'false');
+    });
+    $('#collapsingNavbar').on('show.bs.collapse', function () {
+      $('#collapsingMobileUser').removeClass('in');
+      $('[data-target="#collapsingMobileUser"]').attr('aria-expanded', 'false');
+    });
+    // dark navbar
+    $('#collapsingMobileUserInverse').on('show.bs.collapse', function () {
+      $('#collapsingNavbarInverse').removeClass('in');
+      $('[data-target="#collapsingNavbarInverse"]').attr('aria-expanded', 'false');
+    });
+    $('#collapsingNavbarInverse').on('show.bs.collapse', function () {
+      $('#collapsingMobileUserInverse').removeClass('in');
+      $('[data-target="#collapsingMobileUserInverse"]').attr('aria-expanded', 'false');
+    });
+  }
+
+  function scrollToTop() {
+    $('.scroll-top').on('click', function(e) {
+      e.preventDefault();
+      $.scrollTo('html, body', 1000, {
+        easing:'easeInOutExpo',
+        offset:0,
+        'axis':'y'
+      });
+    });
+    $('.btn-getstarted').on('click', function(e) {
+      e.preventDefault();
+      $.scrollTo('#getstarted', 1000, {
+        easing:'easeInOutExpo',
+        offset:0,
+        'axis':'y'
+      });
+    });
+    $('.btn-team').on('click', function(e) {
+      e.preventDefault();
+      $.scrollTo('#team', 1000, {
+        easing:'easeInOutExpo',
+        offset:0,
+        'axis':'y'
+      });
+    });
+  }
+
+  function carousel() {
+    $(document).ready(function(){
+      $('.owl-carousel').owlCarousel({
+        margin: 50,
+        nav: true,
+        dots: false,
+        autoWidth: true,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
+      });
+    })
+  }
+
+  function headerFixed(){
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 15) {
+        $('.navbar').css("position", "fixed");
+        $('.navbar').addClass("bg-white");
+        $('.navbar').removeClass("bg-inverse-custom");
+        $('.navbar .logo').attr('src', 'assets/img/logo-p.png');
+      } else {
+        $('.navbar').position("position", "absolute");
+        $('.navbar').addClass("bg-inverse-custom");
+        $('.navbar').removeClass("bg-white");
+        $('.navbar .logo').attr('src', 'assets/img/logo-p-white.png');
+      }
+    })
+  }
+
+  function init() {
+    headerFixed();
+    onScrollAnimations();
+    navMobileCollapse();
+    scrollToTop();
+    // carousel();
+  }
+
+  init();
+
+})(jQuery);
+
